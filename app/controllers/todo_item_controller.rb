54451183f0870c33eb832d0ea172f6570ddb7b6f
mattschreiber <matt.schreiber01@gmail.com>
2016-05-31 10:36:12 -0400

    added controllers/views

---
+++ app/controllers/todo_item_controller.rb
@@ -0,0 +1,2 @@
+class TodoItemController < ApplicationController
+end
---
+++ test/controllers/todo_item_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class TodoItemControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
