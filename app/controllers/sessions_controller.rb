86a24880df246bd71fae441bbf9e2eac81085f6d
mattschreiber <matt.schreiber01@gmail.com>
2016-06-01 19:33:21 -0400

    session#create with redirect and flash message

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,18 @@
 class SessionsController < ApplicationController
   def new
+  	#Login page new.html.erb
   end
 
   def create
+  	user = User.find_by(username: params[:user][:username])
+  	password = params[:user][:password]
+
+  	if user && user.authenticate(password)
+  		redirect_to root_path, notice: 'Login Successful.'
+  	else
+  		redirect_to login_path, alert: 'Incorrect Username or Password'
+  	end
+
   end
 
   def destroy
